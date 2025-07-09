776dbffc83748017f1a5fbf23d165007087869d5
Marian <meue@users.noreply.github.com>
2025-07-09 23:31:34 +0200

    installer

--- clean-install-on-pi.sh
+++ clean-install-on-pi.sh
@@ -51,14 +51,8 @@ echo "✅ Installation abgeschlossen. Versionen:"
 git --version
 go version
 
-# mp3 abspielen
-sudo apt install mpg123
+chmod a+rx install.sh
+sudo ./install.sh
 
-# download latest yt-dlp
-curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o src/main/yt-dlp
-chmod a+rx src/main/yt-dlp
-
-# mp3 converter
-sudo apt install ffmpeg:
-
+chmod a+rx install-service.sh
 sudo ./install-service.sh
